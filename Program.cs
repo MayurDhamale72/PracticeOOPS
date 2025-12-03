7f47be5f207ea7a22eacd2385cc49ad1171049c8
Mayur Dhamale <mayur.dhamale@prorigosoftware.com>
2025-12-03 12:02:55 +0530

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Practice
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("FIRST COMMIT");
+    }
+}
